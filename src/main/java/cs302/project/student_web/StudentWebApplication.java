5b8e3df37a1d01a03e38076b96b77e9ed5d19a4c
dgduksict <B211910809@must.edu.mn>
2024-12-09 13:01:49 +0800

    Initial commit

---
+++ src/main/java/cs302/project/student_web/StudentWebApplication.java
@@ -0,0 +1,13 @@
+package cs302.project.student_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs302/project/student_web/StudentWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cs302.project.student_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
